9317ce69b9169b3d3d622d378a0c6334ce343dcd
Ori Sky Farrell <ori.sky.farrell+git@gmail.com>
2022-04-27 19:23:23 +0100

    Move logic into PronounsMod class

--- src/main/java/mx/ori/pronouns/PronounsMod.java
+++ src/main/java/mx/ori/pronouns/PronounsMod.java
@@ -1,26 +1,37 @@
 package mx.ori.pronouns;
 
 import draylar.omegaconfig.OmegaConfig;
+import mx.ori.pronouns.mixin.ChatHudMixin;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.HoverEvent;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Style;
+import net.minecraft.text.TranslatableText;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PronounsMod implements ClientModInitializer {
+	private static final Logger LOGGER = LoggerFactory.getLogger("pronouns");
 	public static final PronounsConfig CONFIG = OmegaConfig.register(PronounsConfig.class);
 
 	public static final HashMap<String, String> pronounsMap = new HashMap<>();
 
+	private static MinecraftClient client = null;
+
 	@Override
 	public void onInitializeClient() {
 		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-			if(screen instanceof TitleScreen) {
+			if (screen instanceof TitleScreen) {
 				var buttons = Screens.getButtons(screen);
 
 				buttons.get(3).y += 12;
@@ -36,10 +47,80 @@ public class PronounsMod implements ClientModInitializer {
 		});
 
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-			if (client.player != null) {
-				String fmt = PronounsMod.CONFIG.pronouns == null ? "#pronouns" : "#pronouns %s";
-				client.player.sendChatMessage(String.format(fmt, PronounsMod.CONFIG.pronouns));
-			}
+			PronounsMod.client = client;
+			broadcast();
 		});
+	}
+
+	public static void broadcast() {
+		assert client.player != null;
+		String fmt = CONFIG.pronouns == null ? "#pronouns" : "#pronouns %s";
+		client.player.sendChatMessage(String.format(fmt, CONFIG.pronouns));
+	}
+
+	public static void whisper(String to) {
+		assert client.player != null;
+		if(!to.equals(client.player.getName().getString())) {
+			String fmt = CONFIG.pronouns == null ? "/tell %s #pronouns" : "/tell %s #pronouns %s";
+			client.player.sendChatMessage(String.format(fmt, to, CONFIG.pronouns));
+		}
+	}
+
+	public static void register(PronounsMessage pm) {
+		if (pm.pronouns() != null) {
+			pronounsMap.put(pm.from(), pm.pronouns());
+			LOGGER.info(String.format("registered pronouns for %s: %s", pm.from(), pm.pronouns()));
+		} else {
+			pronounsMap.remove(pm.from());
+			LOGGER.info(String.format("unregistered pronouns for %s", pm.from()));
+		}
+		replaceBacklog();
+	}
+
+	public static void replaceBacklog() {
+		var chatHud = client.inGameHud.getChatHud();
+		for (var line : ((ChatHudMixin) chatHud).getMessages()) {
+			if (line.getText() instanceof TranslatableText lineText) {
+				replace(lineText);
+			}
+		}
+		chatHud.reset();
+	}
+
+	public static void replace(TranslatableText text) {
+		for (var node : toReplace(text)) {
+			var pronouns = PronounsMod.pronounsMap.get(node.getString());
+			if (pronouns != null) {
+				var tpronouns = new TranslatableText(pronouns);
+				var event = new HoverEvent(HoverEvent.Action.SHOW_TEXT, tpronouns);
+				node.fillStyle(Style.EMPTY.withHoverEvent(event));
+			}
+		}
+	}
+
+	private static @NotNull ArrayList<LiteralText> toReplace(TranslatableText text) {
+		var r = new ArrayList<LiteralText>();
+
+		var key = text.getKey();
+		var indices = MessageType.indexOf(key);
+		var args = text.getArgs();
+
+		for (var index : indices) {
+			if (args[index] instanceof LiteralText node) {
+				r.add(node);
+			}
+		}
+
+		if (key.equals("chat.type.admin")) {
+			if (args[1] instanceof TranslatableText subtext) {
+				r.addAll(toReplace(subtext));
+			}
+		}
+
+		if (indices.isEmpty()) {
+			LOGGER.warn((new IllegalStateException("Unexpected key: " + key)).toString());
+		}
+
+		return r;
 	}
 }
--- src/main/java/mx/ori/pronouns/mixin/InGameHudMixin.java
+++ src/main/java/mx/ori/pronouns/mixin/InGameHudMixin.java
@@ -1,36 +1,20 @@
 package mx.ori.pronouns.mixin;
 
-import mx.ori.pronouns.MessageType;
 import mx.ori.pronouns.PronounsMessage;
 import mx.ori.pronouns.PronounsMod;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.text.*;
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
-    @Shadow
-    @Final
-    private MinecraftClient client;
-
-    @Shadow
-    public abstract ChatHud getChatHud();
-
-    private final Logger LOGGER = LoggerFactory.getLogger("pronouns");
-
     @Inject(at = @At("HEAD"), method = "addChatMessage(Lnet/minecraft/network/MessageType;Lnet/minecraft/text/Text;Ljava/util/UUID;)V", cancellable = true)
     private void addChatMessage(net.minecraft.network.MessageType type, Text message, UUID sender, CallbackInfo ci) {
         if (message instanceof TranslatableText text) {
@@ -38,16 +22,10 @@ public abstract class InGameHudMixin {
             if (pm != null) {
                 // respond via whisper
                 if (pm.shouldRespond()) {
-                    assert client.player != null;
-                    if (!pm.from().equals(client.player.getName().getString())) {
-                        var pronouns = PronounsMod.CONFIG.pronouns;
-                        String fmt = pronouns == null ? "/tell %s #pronouns" : "/tell %s #pronouns %s";
-                        client.player.sendChatMessage(String.format(fmt, pm.from(), pronouns));
-                    }
+                    PronounsMod.whisper(pm.from());
                 }
 
-                register(pm);
-                replaceBacklog();
+                PronounsMod.register(pm);
 
                 ci.cancel();
             } else if (text.getKey().equals("commands.message.display.outgoing")) {
@@ -59,64 +37,7 @@ public abstract class InGameHudMixin {
                 }
             }
 
-            replace(text);
+            PronounsMod.replace(text);
         }
-    }
-
-    private void register(PronounsMessage pm) {
-        if (pm.pronouns() != null) {
-            PronounsMod.pronounsMap.put(pm.from(), pm.pronouns());
-            LOGGER.info(String.format("registered pronouns for %s: %s", pm.from(), pm.pronouns()));
-        } else {
-            PronounsMod.pronounsMap.remove(pm.from());
-            LOGGER.info(String.format("unregistered pronouns for %s", pm.from()));
-        }
-    }
-
-    private void replaceBacklog() {
-        var chatHud = getChatHud();
-        for (var line : ((ChatHudMixin) chatHud).getMessages()) {
-            if (line.getText() instanceof TranslatableText lineText) {
-                replace(lineText);
-            }
-        }
-        chatHud.reset();
-    }
-
-    private void replace(TranslatableText text) {
-        for (var node : toReplace(text)) {
-            var pronouns = PronounsMod.pronounsMap.get(node.getString());
-            if (pronouns != null) {
-                var tpronouns = new TranslatableText(pronouns);
-                var event = new HoverEvent(HoverEvent.Action.SHOW_TEXT, tpronouns);
-                node.fillStyle(Style.EMPTY.withHoverEvent(event));
-            }
-        }
-    }
-
-    private @NotNull ArrayList<LiteralText> toReplace(TranslatableText text) {
-        var r = new ArrayList<LiteralText>();
-
-        var key = text.getKey();
-        var indices = MessageType.indexOf(key);
-        var args = text.getArgs();
-
-        for (var index : indices) {
-            if (args[index] instanceof LiteralText node) {
-                r.add(node);
-            }
-        }
-
-        if (key.equals("chat.type.admin")) {
-            if (args[1] instanceof TranslatableText subtext) {
-                r.addAll(toReplace(subtext));
-            }
-        }
-
-        if (indices.isEmpty()) {
-            LOGGER.warn((new IllegalStateException("Unexpected key: " + key)).toString());
-        }
-
-        return r;
     }
 }
