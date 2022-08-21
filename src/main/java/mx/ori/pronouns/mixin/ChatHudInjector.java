89d52902405ec24ac5b483fd39c7ea08d7d1eb41
Ori Sky Farrell <ori.sky.farrell+git@gmail.com>
2022-08-21 18:05:08 +0100

    Fix Pronouns menu button in presence of mod menu

--- src/main/java/mx/ori/pronouns/mixin/ChatHudInjector.java
+++ src/main/java/mx/ori/pronouns/mixin/ChatHudInjector.java
@@ -5,11 +5,8 @@ import mx.ori.pronouns.PronounsMod;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.MessageIndicator;
 import net.minecraft.network.message.MessageSignatureData;
-import net.minecraft.text.LiteralTextContent;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableTextContent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,13 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatHud.class)
 public class ChatHudInjector {
-    private static final Logger LOGGER = LoggerFactory.getLogger("pronouns");
-
     @Inject(at = @At("HEAD"), method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", cancellable = true)
     private void addMessage(Text text, MessageSignatureData signature, int ticks, MessageIndicator indicator, boolean refresh, CallbackInfo ci) {
         if(!refresh) {
             if(text.getContent() instanceof TranslatableTextContent content) {
-                LOGGER.info(content.getKey());
                 var pm = PronounsMessage.create(text);
                 if(pm != null) {
                     // respond via whisper
